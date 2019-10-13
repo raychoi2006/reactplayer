f0e6db2184fff18af09ba1bcf2abc740278f2e2a
raychoi2006 <raychoi2006@gmail.com>
2019-10-13 22:59:16 +0800

    Updates

---
+++ precache-manifest.42a563f101254c2b972f4244cfaee363.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6553f2424da3acdb50a01c7d85d1987",
+    "url": "/reactplayer/index.html"
+  },
+  {
+    "revision": "6d52476510adb6fa5487",
+    "url": "/reactplayer/static/css/2.af4f3140.chunk.css"
+  },
+  {
+    "revision": "6a8d57e4b2eb60d6ebb7",
+    "url": "/reactplayer/static/css/main.0a8798b3.chunk.css"
+  },
+  {
+    "revision": "6d52476510adb6fa5487",
+    "url": "/reactplayer/static/js/2.7bfddf04.chunk.js"
+  },
+  {
+    "revision": "6a8d57e4b2eb60d6ebb7",
+    "url": "/reactplayer/static/js/main.e74d83dc.chunk.js"
+  },
+  {
+    "revision": "99e0a5abf1d33649b7a5",
+    "url": "/reactplayer/static/js/runtime-main.b51f0126.js"
+  }
+]);
